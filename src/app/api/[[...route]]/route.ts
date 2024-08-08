749debfe619e748f9c9dc1466ddb84d267bb076e
aeropriest <ashok.jaiswal@gmail.com>
2024-08-08 08:00:49 +0800

    * Finished the backedn of hono

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,3 +1,7 @@
+import {z} from "zod"
+import { zValidator } from "@hono/zod-validator"
+import { clerkMiddleware, getAuth  } from "@hono/clerk-auth"
+
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
@@ -5,11 +9,27 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
-  })
+app
+.get('/hello', 
+    clerkMiddleware(),
+    (c) => {
+        const auth = getAuth(c);
+        if( !auth?.userId)
+            return c.json({error: 'Unathorized'})
+        return c.json({
+            message: 'Hello Next.js!',
+            userId: auth?.userId,
+        })
 })
-
+.post("/", 
+    zValidator("json", z.object({
+        name: z.string(),
+        userId: z.number(),
+    })), 
+    (c) =>{
+        const {name, userId} = c.req.valid('json');
+        return c.json({});
+})
+  
 export const GET = handle(app)
 export const POST = handle(app)
